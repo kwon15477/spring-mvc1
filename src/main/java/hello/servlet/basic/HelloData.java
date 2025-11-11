8623671c50d24968ce9c2b16595cb9e45a03ed55
kwon15477 <kbj7072@naver.com>
2025-11-11 21:18:15 +0900

    서블릿

---
+++ src/main/java/hello/servlet/basic/HelloData.java
@@ -0,0 +1,14 @@
+package hello.servlet.basic;
+
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter //lombok
+@Setter
+public class HelloData {
+
+    private String username;
+    private int age;
+
+}
