2aa256ae8149d59ebce93c9cd9feaadbdc240831
kwon15477 <kbj7072@naver.com>
2025-11-24 18:25:45 +0900

    서블릿, JSP, MVC 패턴

---
+++ src/test/java/hello/servlet/domain/member/MemberRepositoryTest.java
@@ -0,0 +1,55 @@
+package hello.servlet.domain.member;
+
+import hello.servlet.member.Member;
+import hello.servlet.member.MemberRepository;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class MemberRepositoryTest {
+    MemberRepository memberRepository = MemberRepository.getInstance();
+
+    // 테스트 격리
+    @AfterEach
+    void afterEach() {
+        memberRepository.clearStore(); // 저장소 초기화
+
+    }
+
+    @Test
+    void save() {
+        // given
+        Member member = new Member("hello", 20);
+
+        // when
+        Member saveMember = memberRepository.save(member);
+
+        // then
+        Member findMember = memberRepository.findById(saveMember.getId());
+        assertThat(findMember).isEqualTo(saveMember);
+
+    }
+
+
+    @Test
+    void findAll() {
+        // given
+        Member member1 = new Member("member1", 20);
+        Member member2 = new Member("member2", 30);
+
+        memberRepository.save(member1);
+        memberRepository.save(member2);
+
+        // when
+        List<Member> result = memberRepository.findAll();
+
+        //  then
+        assertThat(result.size()).isEqualTo(2);
+        assertThat(result).contains(member1, member2);
+
+    }
+}
